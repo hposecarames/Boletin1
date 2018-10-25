5f7c5b2287092eba00c9dfc1f3cfafd33209b313
hposecarames <menuven@trapallada>
2018-10-25 09:02:35 +0200

    Boletin 1_4 código simplificado

--- Boletin1_4/src/boletin1_4/Boletin1_4.java
+++ Boletin1_4/src/boletin1_4/Boletin1_4.java
@@ -8,21 +8,12 @@ public class Boletin1_4 {
     
     public static void main(String[] args) {
     
-        double num1;
-        double num2;
-        double suma, resta, multiplicación,división;
+        double num1, num2;
         num1 = Double.parseDouble(JOptionPane.showInputDialog("Introduce num1: "));
         num2 = Double.parseDouble(JOptionPane.showInputDialog("Introduce num2: "));
         
-        suma = num1 + num2;
-        resta = num1 - num2;
-        multiplicación = num1 * num2;
-        división = num1 / num2;
-        
-        JOptionPane.showMessageDialog(null, "num1 + num2: "+suma+"\nnum1 - num2: "+resta+"\nnum1 * num2: "+multiplicación+"\nnum1 / num2: "+división);
-       
-        
-        
+        JOptionPane.showMessageDialog(null, "num1 + num2: "+(num1 + num2)+"\nnum1 - num2: "+(num1 - num2)+"\nnum1 * num2: "+(num1 * num2)+"\nnum1 / num2: "+(num1 / num2));
+                 
     }
     
 }
