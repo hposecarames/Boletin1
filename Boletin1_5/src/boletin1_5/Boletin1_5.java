8ed810b3c7751a82213329c4120e1cb83749afa0
hposecarames <menuven@trapallada>
2018-10-25 09:04:36 +0200

    Boletin 1_5 código simplificado

--- Boletin1_5/src/boletin1_5/Boletin1_5.java
+++ Boletin1_5/src/boletin1_5/Boletin1_5.java
@@ -8,15 +8,12 @@ public class Boletin1_5 {
     
     public static void main(String[] args) {
        
-        double valorMilla;
-        double milla;
-        double metros;
-        
+        double valorMilla, milla;
+                
         valorMilla = 1852;
         milla = Double.parseDouble(JOptionPane.showInputDialog("Introduce las millas: "));
-        metros = milla * valorMilla;
         
-        JOptionPane.showMessageDialog(null, milla+" millas son "+metros+"m");
+        JOptionPane.showMessageDialog(null, milla+" millas son "+milla * valorMilla+"m");
         
     }
     
